d8fcebbbdc69b12f588e66e3b7564e2ef303eda5
Henrique Wegner <henriqueawegner@hotmail.com>
2022-09-23 15:51:02 -0300

    First Commit + SpringBoot + Entity Client

---
+++ src/main/java/io/github/HenriqueWegner/ClientsApp/model/entity/Client.java
@@ -0,0 +1,20 @@
+package io.github.HenriqueWegner.ClientsApp.model.entity;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+
+@Entity
+public class Client {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(nullable = false, length = 150)
+    private String name;
+
+    @Column(nullable = false, length = 11)
+    private String cpf;
+
+    @Column
+    private LocalDate registrationDate;
+}
