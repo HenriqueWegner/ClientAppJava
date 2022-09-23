f475e6dd4e1d90b814b77f5a22796f54b688700f
Henrique Wegner <henriqueawegner@hotmail.com>
2022-09-23 16:05:32 -0300

    Lombok added to Pom + Service Entity

--- src/main/java/io/github/HenriqueWegner/ClientsApp/model/entity/Client.java
+++ src/main/java/io/github/HenriqueWegner/ClientsApp/model/entity/Client.java
@@ -1,9 +1,14 @@
 package io.github.HenriqueWegner.ClientsApp.model.entity;
 
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+
 import javax.persistence.*;
 import java.time.LocalDate;
 
 @Entity
+@Data
 public class Client {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -15,6 +20,7 @@ public class Client {
     @Column(nullable = false, length = 11)
     private String cpf;
 
-    @Column
+    @Column(name = "registration_date")
     private LocalDate registrationDate;
+
 }
