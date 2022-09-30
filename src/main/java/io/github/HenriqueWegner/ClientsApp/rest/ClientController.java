b45d3ba965a0069a4236ddfd2d1e18ab8d34a592
Henrique Wegner <henriqueawegner@hotmail.com>
2022-09-30 17:37:44 -0300

    Added DELETE and PUT

--- src/main/java/io/github/HenriqueWegner/ClientsApp/rest/ClientController.java
+++ src/main/java/io/github/HenriqueWegner/ClientsApp/rest/ClientController.java
@@ -31,4 +31,28 @@ public class ClientController {
                 .orElseThrow( () -> new ResponseStatusException(HttpStatus.NOT_FOUND) );
     }
 
+    @DeleteMapping("{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void delete(@PathVariable Integer id){
+        repository
+                .findById(id)
+                .map(client -> {
+                    repository.delete(client);
+                    return Void.TYPE;
+        })
+                .orElseThrow( () -> new ResponseStatusException(HttpStatus.NOT_FOUND));
+    }
+
+    @PutMapping("{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void update(@PathVariable Integer id, @RequestBody Client updatedClient){
+        repository
+                .findById(id)
+                .map( client -> {
+                    updatedClient.setId(client.getId());
+                    return repository.save(updatedClient);
+                })
+                .orElseThrow( () -> new ResponseStatusException(HttpStatus.NOT_FOUND));
+    }
+
 }
